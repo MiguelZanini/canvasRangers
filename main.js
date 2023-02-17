767b1a497568034a450fca7955a56720ebac9157
MiguelZanini <112401886+MiguelZanini@users.noreply.github.com>
2023-02-17 11:26:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+
+var canvas = new fabric.Canvas('myCanvas');
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+
+	function new_image (getImage)
+{
+fabric.Image.fromURL(getImage, function (Img) {blockImageObject = Img;
+ blockImageObject.scaleToWidth (blockImageWidth);
+ blockImageObject.scaleToHeight (blockImageHeight);
+ blockImageObject.set({
+top:blockY,left:blockX
+});
+canvas.add(blockImageObject);
+});
+}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	
+
+	
+		if(keyPressed == '82') 
+	{
+	new_image('rr1.png');
+	console.log("r");
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		new_image('gr.png')
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		new_image('yr.png')
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		new_image('pr.png')
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+		new_image('br.png')
+	}
+}
+
+
